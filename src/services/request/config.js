d17cf4b3301e47ee0a00a466068c9f0d4fd9e27b
charlotte <chlottedu@gmail.com>
2025-03-17 16:47:09 +0800

    fix api url

--- src/services/request/config.js
+++ src/services/request/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://cors-anywhere.herokuapp.com/http://codercba.com:1888/airbnb/api"
+export const BASE_URL = "https://my-cors-proxy-five.vercel.app/api/proxy?url=http://codercba.com:1888/airbnb/api"
 export const TIMEOUT = 10000
